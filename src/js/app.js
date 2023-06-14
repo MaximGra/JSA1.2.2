d1bd1382af213c124b6730131a1880763429077f
Grachev Maksim <magra85@gmail.com>
2023-06-15 00:23:31 +0300

    Added HW 1.2.2

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import Game,
+{
+  GameSavingData,
+  readGameSaving as loadGame,
+  writeGameSaving as saveGame,
+}
+
+  from './game';
+
+const game = new Game();
+game.start();
+
+console.log('app worked');
